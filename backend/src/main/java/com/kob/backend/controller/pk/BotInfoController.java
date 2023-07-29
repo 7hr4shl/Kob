b52032a51f33b6beadbbc91cfc5feec5d203cbaf
Shenghan Yin <yshenghan@yahoo.com>
2023-07-28 19:47:04 -0600

    项目骨架搭建完成

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -12,20 +12,14 @@ import java.util.Map;
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public List<Map<String,String>> getBotInfo(){
+    public Map<String,String> getBotInfo(){
         List<Map<String,String>> list = new LinkedList<>();
 
         Map<String,String> bot1 = new HashMap<>();
         bot1.put("name","tiger");
         bot1.put("rating","2500");
 
-        Map<String,String> bot2 = new HashMap<>();
-        bot2.put("name","lion");
-        bot2.put("rating","2300");
 
-        list.add(bot1);
-        list.add(bot2);
-
-        return list;
+        return bot1;
     }
 }
